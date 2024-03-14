6553358bfc63630ff988a98f4bdb6bcbb5b405b8
EmanZR <emerick@rtnet.fr>
2024-03-14 18:49:15 +0100

    First scripts

---
+++ client/rtgui.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+# This script will use gs.py functions in order to create a GUI for the user to interact with the system.
+
+import tkinter as tk
+from tkinter import ttk
+from tkinter import messagebox
+import gs
+import time
+import threading
+
+window = tk.Tk()
+window.title("RTGUI for RTMSG")
+window.geometry("400x400")
+
+user = tk.StringVar()
+launch = tk.Button(window, text="Authenticate", command=gs.auth)
+launch.pack()
+
+window.mainloop()
+
