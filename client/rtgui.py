db5a0a60e36359255b74435267a09aee4fe930dc
EmanZR <emerick@rtnet.fr>
2024-03-15 09:24:52 +0100

    Going further

--- client/rtgui.py
+++ client/rtgui.py
@@ -8,6 +8,17 @@ import gs
 import time
 import threading
 
+window = tk.Tk()
+window.title("RTGUI for RTMSG")
+window.geometry("600x400")
+
+entry = tk.Entry(window)
+entry.pack()
+username = entry.get()
+
+user = tk.StringVar()
+
+
 def assign_username():
     username = entry.get()
     return username
@@ -16,21 +27,19 @@ def call_gs():
     username = assign_username()
     logged = gs.auth(username)
     if logged:
-        message.config(text="Bienvenue "+username+" !")
+        message = tk.Label(window, text="Bienvenue "+username+" !")
+        message.pack()
+        time.sleep(5)
+        user_gui()
     else:
-        message.config(text="Impossible de vous authentifier")
+        message = tk.Label(window, text="Impossible de vous authentifier !")
+        message.pack()
 
-window = tk.Tk()
-window.title("RTGUI for RTMSG")
-window.geometry("400x400")
+def user_gui():
+    launch.pack_forget()
+    message.pack_forget()
+    entry.pack_forget()
 
-entry = tk.Entry(window)
-entry.pack()
-username = entry.get()
-
-message = tk.Label(window, text="")
-
-user = tk.StringVar()
 launch = tk.Button(window, text="Authenticate", command=call_gs)
 launch.pack()
 
