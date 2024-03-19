2238ae181b4af29f93a44498dcf596f9e0227689
EmanZR <emerick@rtnet.fr>
2024-03-19 09:51:40 +0100

    Adding libraries

--- server/api.py
+++ server/api.py
@@ -8,6 +8,7 @@ from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.primitives.asymmetric import padding
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.backends import default_backend
+import argparse
 import os
 import csv
 import jwt
